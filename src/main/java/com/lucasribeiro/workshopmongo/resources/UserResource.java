8dc897cfe402bffd318cc96710acaca4cc48d3f9
Luqui05 <lucashtribeiro@hotmail.com>
2024-04-19 10:16:16 -0300

    Inserção de usuário com POST

--- src/main/java/com/lucasribeiro/workshopmongo/resources/UserResource.java
+++ src/main/java/com/lucasribeiro/workshopmongo/resources/UserResource.java
@@ -6,11 +6,10 @@ import com.lucasribeiro.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -32,5 +31,17 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO) {
+        User obj = service.fromDTO(objDTO);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(obj.getId())
+                .toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/lucasribeiro/workshopmongo/services/UserService.java
+++ src/main/java/com/lucasribeiro/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.lucasribeiro.workshopmongo.services;
 
 import com.lucasribeiro.workshopmongo.domain.User;
+import com.lucasribeiro.workshopmongo.dto.UserDTO;
 import com.lucasribeiro.workshopmongo.repository.UserRepository;
 import com.lucasribeiro.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
